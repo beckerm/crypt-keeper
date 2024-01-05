fe498bfd32bdb1a77607442f8c248dd8f7d0dc66
beckerm <markabecker@gmail.com>
2024-01-05 11:09:13 -0800

    updates

--- crypty.py
+++ crypty.py
@@ -2,6 +2,7 @@
 
 from cryptography.fernet import Fernet
 import argparse
+import os
 
 
 parser = argparse.ArgumentParser(description='Encrypt and decrypt files.')
@@ -25,65 +26,78 @@ def create_key(n):
 
 def encrypt_file(f, k):
 
-    try:
-        with open(k, 'rb') as filekey:
-            key = filekey.read()
-    except Exception as e:
-        print(f"Error: {e}")
-        exit()
+    if os.path.exists(k):
 
-    fernet = Fernet(key)
+        try:
+            with open(k, 'rb') as filekey:
+                key = filekey.read()
+        except Exception as e:
+            print(f"Error: {e}")
+            exit()
 
-    try:
-        with open(f, 'rb') as file:
-            original = file.read()
-    except Exception as e:
-        print(f"Error: {e}")
-        exit()
+        fernet = Fernet(key)
 
-    encrypted = fernet.encrypt(original)
+        try:
+            with open(f, 'rb') as file:
+                original = file.read()
+        except Exception as e:
+            print(f"Error: {e}")
+            exit()
 
-    try:
-        with open(f, 'wb') as encrypted_file:
-            encrypted_file.write(encrypted)
-    except Exception as e:
-        print(f"Error: {e}")
-        exit()
+        encrypted = fernet.encrypt(original)
+
+        try:
+            with open(f, 'wb') as encrypted_file:
+                encrypted_file.write(encrypted)
+        except Exception as e:
+            print(f"Error: {e}")
+            exit()
+
+    else:
+        print(f"Error: Key file '{k}' not found.")
 
 
 def decrypt_file(f, k):
 
-    try:
-        with open(k, 'rb') as filekey:
-            key = filekey.read()
-    except Exception as e:
-        print(f"Error: {e}")
+    if os.path.exists(k):
+
+        try:
+            with open(k, 'rb') as filekey:
+                key = filekey.read()
+        except Exception as e:
+            print(f"Error: {e}")
+            exit()
+
+        fernet = Fernet(key)
+
+        try:
+            with open(f, 'rb') as encrypted_file:
+                encrypted = encrypted_file.read()
+        except Exception as e:
+            print(f"Error: {e}")
+            exit()
+
+        decrypted = fernet.decrypt(encrypted)
+
+        try:
+            with open(f, 'wb') as decrypted_file:
+                decrypted_file.write(decrypted)
+        except Exception as e:
+            print(f"Error: {e}")
+            exit()
+
+    else:
+        print(f"Error: Key file '{k}' not found.")
+
+
+
+if __name__ == "__main__":
+
+    if args.makekey:
+        create_key(args.makekey)
+    elif args.encrypt and args.key:
+        encrypt_file(args.encrypt, args.key)
+    elif args.decrypt and args.key:
+        decrypt_file(args.decrypt, args.key)
+    else:
         exit()
-
-    fernet = Fernet(key)
-
-    try:
-        with open(f, 'rb') as encrypted_file:
-            encrypted = encrypted_file.read()
-    except Exception as e:
-        print(f"Error: {e}")
-        exit()
-
-    decrypted = fernet.decrypt(encrypted)
-
-    try:
-        with open(f, 'wb') as decrypted_file:
-            decrypted_file.write(decrypted)
-    except Exception as e:
-        print(f"Error: {e}")
-        exit()
-
-
-if args.makekey:
-    create_key(args.makekey)
-elif args.encrypt and args.key:
-    encrypt_file(args.encrypt, args.key)
-elif args.decrypt and args.key:
-    decrypt_file(args.decrypt, args.key)
-else:
-    exit()
